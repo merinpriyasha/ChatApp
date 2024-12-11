da9cb8201903df49a6ffe372e966b9ba1e41a6d4
merinpriyasha <merinpriyasha@gmail.com>
2024-12-11 16:40:40 +0530

    create filter-users endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongodb.js";
-import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -15,6 +18,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, ()=>{
     connectToMongoDB();
