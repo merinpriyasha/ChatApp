fe909032c627aebc556cc894590a8cfe933101a6
merinpriyasha <merinpriyasha@gmail.com>
2024-12-14 13:36:15 +0530

    create deployment configuration

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -10,6 +11,8 @@ import connectToMongoDB from "./db/connectToMongodb.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
+
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -18,6 +21,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, ()=>{
     connectToMongoDB();
